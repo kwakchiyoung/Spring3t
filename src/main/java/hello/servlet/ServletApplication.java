ecdbe055b7c42bcaa85a3983ba04d16fda68df62
Luca Kwak2 <cldud03@naver.com>
2023-02-22 02:42:18 +0900

    init2

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //스프링 부트는 서블릿을 직접 등록해서 사용할 수 있도록 @ServletComponentScan 을 지원한다. //서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
