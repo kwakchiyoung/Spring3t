518a36dd6279abb57d4b5592216d33cd28c0bf26
Luca Kwak <cldud03@naver.com>
2023-02-27 15:30:18 +0900

    MVC프레임워크 만들기 모델추가 MVC3(컨트롤러에서 서블릿을 모델로사용)-그램-2

--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -21,8 +21,7 @@ public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
     private Map<String, String> createParamMap(HttpServletRequest request) {
         Map<String, String> paramMap = new HashMap<>();
         request.getParameterNames().asIterator()
-                .forEachRemaining(paramName -> paramMap.put(paramName,
-                        request.getParameter(paramName)));
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
         return paramMap;
     }
 }
